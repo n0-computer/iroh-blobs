883e964e3da7a5f9521e077be9771c4063663d3a
dignifiedquire <dignifiedquire@users.noreply.github.com>
2024-10-25 13:46:34 +0000

    deploy: 07cdebf38d74806627fb0e0bb04ca0d456a0d16a

---
+++ pr/5/docs/trait.impl/iroh_metrics/core/trait.Metric.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_blobs":[["impl Metric for <a class=\"struct\" href=\"iroh_blobs/metrics/struct.Metrics.html\" title=\"struct iroh_blobs::metrics::Metrics\">Metrics</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
