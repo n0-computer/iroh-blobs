fab4e1da1d0e8c6a08961bc1509f21a27e19ab6b
rklaehn <rklaehn@users.noreply.github.com>
2024-11-18 08:50:08 +0000

    deploy: 7774330049036bf7d8636ff7bd49933b37b9b9d6

---
+++ pr/13/docs/trait.impl/clap_builder/derive/trait.Args.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_blobs":[["impl Args for <a class=\"struct\" href=\"iroh_blobs/cli/struct.BlobAddOptions.html\" title=\"struct iroh_blobs::cli::BlobAddOptions\">BlobAddOptions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
