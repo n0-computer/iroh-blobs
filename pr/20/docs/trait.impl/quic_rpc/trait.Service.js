943be190974f206503426f5a5a6dc8959061a581
rklaehn <rklaehn@users.noreply.github.com>
2024-11-28 16:01:42 +0000

    deploy: b98c949fbfe05f7c2664432febeace482536ffd4

---
+++ pr/20/docs/trait.impl/quic_rpc/trait.Service.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_blobs":[["impl Service for <a class=\"struct\" href=\"iroh_blobs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_blobs::rpc::proto::RpcService\">RpcService</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
