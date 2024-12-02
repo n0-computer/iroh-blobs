8d6cf9416832f1331ef577fff6666ccc64a9c064
rklaehn <rklaehn@users.noreply.github.com>
2024-12-02 13:26:27 +0000

    deploy: 7be230a57726b43360f9b2c7ff66682dd1d3e1f2

---
+++ pr/24/docs/trait.impl/iroh/protocol/trait.ProtocolHandler.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_blobs":[["impl ProtocolHandler for <a class=\"struct\" href=\"iroh_blobs/net_protocol/struct.Blobs.html\" title=\"struct iroh_blobs::net_protocol::Blobs\">Blobs</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
