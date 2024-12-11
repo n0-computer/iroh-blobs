7a167ded94ec8df22b4365f4b63378d7af95fff0
rklaehn <rklaehn@users.noreply.github.com>
2024-12-11 10:19:34 +0000

    deploy: d5b525b44061e08431efcae1d0d32d803ed63de3

--- pr/32/docs/iroh_blobs/downloader/sidebar-items.js
+++ pr/32/docs/iroh_blobs/downloader/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DownloadError","FailureAction","GetOutput"],"struct":["ConcurrencyLimits","DownloadHandle","DownloadKind","DownloadRequest","Downloader","IntentId","RetryConfig"],"trait":["Dialer","Getter","NeedsConn"]};
+window.SIDEBAR_ITEMS = {"enum":["DownloadError","FailureAction","GetOutput"],"struct":["ConcurrencyLimits","DownloadHandle","DownloadKind","DownloadRequest","Downloader","IntentId","RetryConfig"],"trait":["Getter","NeedsConn"]};
