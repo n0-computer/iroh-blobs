1c81549c3867d689f49cd3575be55dc63298e679
rklaehn <rklaehn@users.noreply.github.com>
2024-12-11 11:14:10 +0000

    deploy: 4b13eda61947a738a83fc50e677fa201494c6f2f

---
+++ pr/37/docs/src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["iroh_blobs",["",[["cli",[],["tags.rs"]],["downloader",[],["get.rs","invariants.rs","progress.rs"]],["format",[],["collection.rs"]],["get",[],["db.rs","error.rs","progress.rs","request.rs"]],["protocol",[],["range_spec.rs"]],["rpc",[["client",[["blobs",[],["batch.rs"]]],["blobs.rs","tags.rs"]],["proto",[],["blobs.rs","tags.rs"]]],["client.rs","proto.rs"]],["store",[["fs",[],["tables.rs","test_support.rs","util.rs","validate.rs"]]],["bao_file.rs","fs.rs","mem.rs","mutable_mem_storage.rs","readonly_mem.rs","traits.rs"]],["util",[],["fs.rs","io.rs","local_pool.rs","mem_or_file.rs","progress.rs","sparse_mem_file.rs"]]],["cli.rs","downloader.rs","export.rs","format.rs","get.rs","hashseq.rs","lib.rs","metrics.rs","net_protocol.rs","protocol.rs","provider.rs","rpc.rs","store.rs","util.rs"]]]\
+]'));
+createSrcSidebar();
---
+++ pr/37/docs/trait.impl/tokio/io/async_write/trait.AsyncWrite.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"iroh_blobs":[["impl&lt;W: AsyncWrite + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>&gt; AsyncWrite for <a class=\"struct\" href=\"iroh_blobs/util/io/struct.TrackingWriter.html\" title=\"struct iroh_blobs::util::io::TrackingWriter\">TrackingWriter</a>&lt;W&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
