04291b5ec8306650ef7523ec5bf013d59f2765ca
rklaehn <rklaehn@users.noreply.github.com>
2025-03-11 09:00:35 +0000

    deploy: bc21b379fa2ca220fbfb902495a72e7005c88a2b

---
+++ pr/70/docs/trait.impl/quic_rpc/pattern/bidi_streaming/trait.BidiStreamingMsg.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl BidiStreamingMsg&lt;<a class=\"struct\" href=\"iroh_blobs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_blobs::rpc::proto::RpcService\">RpcService</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/rpc/proto/blobs/struct.AddStreamRequest.html\" title=\"struct iroh_blobs::rpc::proto::blobs::AddStreamRequest\">AddStreamRequest</a>"],["impl BidiStreamingMsg&lt;<a class=\"struct\" href=\"iroh_blobs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_blobs::rpc::proto::RpcService\">RpcService</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/rpc/proto/blobs/struct.BatchAddStreamRequest.html\" title=\"struct iroh_blobs::rpc::proto::blobs::BatchAddStreamRequest\">BatchAddStreamRequest</a>"],["impl BidiStreamingMsg&lt;<a class=\"struct\" href=\"iroh_blobs/rpc/proto/struct.RpcService.html\" title=\"struct iroh_blobs::rpc::proto::RpcService\">RpcService</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/rpc/proto/blobs/struct.BatchCreateRequest.html\" title=\"struct iroh_blobs::rpc::proto::blobs::BatchCreateRequest\">BatchCreateRequest</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1081]}
