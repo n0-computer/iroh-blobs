f316fc9a73a050797c8ad1c2f34091827a5118e5
rklaehn <rklaehn@users.noreply.github.com>
2025-03-14 08:26:16 +0000

    deploy: ffd0c6538f9cf22172b10716360ba7e707a61ff2

--- pr/76/docs/iroh_blobs/net_protocol/sidebar-items.js
+++ pr/76/docs/iroh_blobs/net_protocol/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["DownloadMode"],"struct":["BatchId","BlobDownloadRequest","Blobs","Builder","DownloaderConfig"],"type":["ProtectCb"]};
+window.SIDEBAR_ITEMS = {"enum":["DownloadMode"],"struct":["BatchId","BlobDownloadRequest","Blobs","Builder"],"type":["ProtectCb"]};
