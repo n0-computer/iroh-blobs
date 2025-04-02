5833e78229d15881ef5e3287f75a5adff745dfbd
rklaehn <rklaehn@users.noreply.github.com>
2025-04-02 06:41:41 +0000

    deploy: ae8a3e180c7df75d64ecfc6a095c784aae963200

---
+++ pr/81/docs/trait.impl/core/convert/trait.AsRef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_blobs/struct.Hash.html\" title=\"struct iroh_blobs::Hash\">Hash</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_blobs/util/struct.SparseMemFile.html\" title=\"struct iroh_blobs::util::SparseMemFile\">SparseMemFile</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"iroh_blobs/util/struct.Tag.html\" title=\"struct iroh_blobs::util::Tag\">Tag</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1130]}
