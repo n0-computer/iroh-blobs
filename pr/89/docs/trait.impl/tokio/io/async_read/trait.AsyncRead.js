fede2aa1b030aef0e987b6451f1512b6b20a89b9
rklaehn <rklaehn@users.noreply.github.com>
2025-05-05 12:06:48 +0000

    deploy: 872338b2b1576970a0df646bcd3469ff7c852b80

---
+++ pr/89/docs/trait.impl/tokio/io/async_read/trait.AsyncRead.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl AsyncRead for <a class=\"struct\" href=\"iroh_blobs/rpc/client/blobs/struct.Reader.html\" title=\"struct iroh_blobs::rpc::client::blobs::Reader\">Reader</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[182]}
