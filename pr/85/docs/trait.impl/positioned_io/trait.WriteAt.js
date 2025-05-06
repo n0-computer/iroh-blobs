8dc9441da986eccb87a0a14d99eeb358a28da5b4
Frando <Frando@users.noreply.github.com>
2025-05-06 21:36:55 +0000

    deploy: 900a9458d1f29a16c6749c4ed80a3a141f58ba60

--- pr/85/docs/trait.impl/positioned_io/trait.WriteAt.js
+++ pr/85/docs/trait.impl/positioned_io/trait.WriteAt.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://docs.rs/positioned-io/0.3.3/positioned_io/trait.WriteAt.html\" title=\"trait positioned_io::WriteAt\">WriteAt</a> for <a class=\"struct\" href=\"iroh_blobs/util/struct.SparseMemFile.html\" title=\"struct iroh_blobs::util::SparseMemFile\">SparseMemFile</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://docs.rs/positioned-io/0.3.4/positioned_io/trait.WriteAt.html\" title=\"trait positioned_io::WriteAt\">WriteAt</a> for <a class=\"struct\" href=\"iroh_blobs/util/struct.SparseMemFile.html\" title=\"struct iroh_blobs::util::SparseMemFile\">SparseMemFile</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
