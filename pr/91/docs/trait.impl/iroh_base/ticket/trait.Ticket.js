18c8217e6d2ec9b33d483e2595fa7ad8ee9d58d0
ramfox <ramfox@users.noreply.github.com>
2025-05-12 15:08:22 +0000

    deploy: 099472212ac3748a1b82bd2f1d7cb9a3659d2df6

---
+++ pr/91/docs/trait.impl/iroh_base/ticket/trait.Ticket.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl Ticket for <a class=\"struct\" href=\"iroh_blobs/ticket/struct.BlobTicket.html\" title=\"struct iroh_blobs::ticket::BlobTicket\">BlobTicket</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[169]}
