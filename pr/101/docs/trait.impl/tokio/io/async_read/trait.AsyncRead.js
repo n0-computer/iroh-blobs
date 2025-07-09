a2a8fcc68161fd13343fa36389b5601e3a073cdd
rklaehn <rklaehn@users.noreply.github.com>
2025-07-09 09:33:37 +0000

    deploy: 7a42e1677d35441f457a34faaf4bf45b43757e17

---
+++ pr/101/docs/trait.impl/tokio/io/async_read/trait.AsyncRead.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl&lt;R: AsyncRead + <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a>&gt; AsyncRead for <a class=\"struct\" href=\"iroh_blobs/provider/struct.CountingReader.html\" title=\"struct iroh_blobs::provider::CountingReader\">CountingReader</a>&lt;R&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[356]}
