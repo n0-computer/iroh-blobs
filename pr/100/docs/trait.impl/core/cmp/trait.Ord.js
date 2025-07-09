f045546059b78c37c103332667facaa84d12cc04
rklaehn <rklaehn@users.noreply.github.com>
2025-07-09 08:32:31 +0000

    deploy: 5c37eaa5eb02e1b48b969372443b2d8201970332

---
+++ pr/100/docs/trait.impl/core/cmp/trait.Ord.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"iroh_blobs/enum.BlobFormat.html\" title=\"enum iroh_blobs::BlobFormat\">BlobFormat</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"iroh_blobs/api/struct.Tag.html\" title=\"struct iroh_blobs::api::Tag\">Tag</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"iroh_blobs/struct.Hash.html\" title=\"struct iroh_blobs::Hash\">Hash</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"iroh_blobs/struct.HashAndFormat.html\" title=\"struct iroh_blobs::HashAndFormat\">HashAndFormat</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1013]}
