f3629844bcd6d44ca25ecb72f32c482d2d4caf75
Frando <Frando@users.noreply.github.com>
2025-07-28 08:12:23 +0000

    deploy: 1698ed8b5c904e517d384eff3c4041410e90c987

---
+++ pr/128/docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"iroh_blobs/api/struct.TempTag.html\" title=\"struct iroh_blobs::api::TempTag\">TempTag</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[286]}
