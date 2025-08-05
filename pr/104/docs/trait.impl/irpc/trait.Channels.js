e75ea28f94d5adf56c99f4cf4b5117fa2d641eda
rklaehn <rklaehn@users.noreply.github.com>
2025-08-05 09:15:00 +0000

    deploy: f9e6e01c8bff59734ccba7f6402b5ea4613f2cad

--- pr/104/docs/trait.impl/irpc/trait.Channels.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["iroh_blobs",[["impl Channels&lt;<a class=\"struct\" href=\"iroh_blobs/api/downloader/struct.DownloaderService.html\" title=\"struct iroh_blobs::api::downloader::DownloaderService\">DownloaderService</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/api/downloader/struct.DownloadRequest.html\" title=\"struct iroh_blobs::api::downloader::DownloadRequest\">DownloadRequest</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[381]}
--- pr/104/docs/trait.impl/irpc/trait.Service.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["iroh_blobs",[["impl Service for <a class=\"struct\" href=\"iroh_blobs/api/downloader/struct.DownloaderService.html\" title=\"struct iroh_blobs::api::downloader::DownloaderService\">DownloaderService</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[208]}
