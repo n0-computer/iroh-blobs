91e5945b899021d683b94d9c1734da286416e977
rklaehn <rklaehn@users.noreply.github.com>
2025-09-02 08:03:07 +0000

    deploy: 0d6fa23a3385c331135aaedc3e4305d25b7175c2

---
+++ pr/142/docs/trait.impl/postcard/max_size/trait.MaxSize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl MaxSize for <a class=\"enum\" href=\"iroh_blobs/enum.BlobFormat.html\" title=\"enum iroh_blobs::BlobFormat\">BlobFormat</a>"],["impl MaxSize for <a class=\"enum\" href=\"iroh_blobs/protocol/enum.RequestType.html\" title=\"enum iroh_blobs::protocol::RequestType\">RequestType</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_blobs/struct.Hash.html\" title=\"struct iroh_blobs::Hash\">Hash</a>"],["impl MaxSize for <a class=\"struct\" href=\"iroh_blobs/struct.HashAndFormat.html\" title=\"struct iroh_blobs::HashAndFormat\">HashAndFormat</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[573]}
