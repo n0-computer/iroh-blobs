eba9d66b60ce50df694812335caa58c7f9d01739
b5 <b5@users.noreply.github.com>
2025-10-03 03:51:22 +0000

    deploy: d09cf7a0ca3562591e29bd7a41e3e12bdc016cd2

---
+++ pr/178/docs/iroh_blobs/api/downloader/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["DownloadProgressItem","FiniteRequest","SplitStrategy"],"struct":["AddProviderRequest","DownloadProgress","DownloadRequest","Downloader","Shuffled"],"trait":["ContentDiscovery","SupportedRequest"],"type":["DownloadMsg","DownloadOptions"]};
