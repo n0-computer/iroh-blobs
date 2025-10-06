e8b2fb9282cf980b562ddc8f22f53920436221fa
rklaehn <rklaehn@users.noreply.github.com>
2025-10-06 12:56:32 +0000

    deploy: 31d57eb041a1d8ab4953bf1a7bad0571695d2caf

--- pr/176/docs/iroh_blobs/provider/sidebar-items.js
+++ pr/176/docs/iroh_blobs/provider/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["handle_connection","handle_get","handle_get_many","handle_observe","handle_push"],"mod":["events"],"struct":["ProgressReader","ProgressWriter","StreamPair","TransferStats"]};
+window.SIDEBAR_ITEMS = {"enum":["HandleGetError","HandleGetManyError","HandleObserveError","HandlePushError"],"fn":["handle_connection","handle_get","handle_get_many","handle_observe","handle_push","handle_stream"],"mod":["events"],"struct":["ProgressReader","ProgressWriter","StreamPair","TransferStats"],"trait":["ErrorHandler"]};
