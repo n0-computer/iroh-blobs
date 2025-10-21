e3b370fc301badd260df5df2fe90a15b2f32fa4e
Frando <Frando@users.noreply.github.com>
2025-10-21 15:27:00 +0000

    deploy: d2a4f006140c412b9c40d03f1cfa1f267e4abc4a

---
+++ pr/187/docs/trait.impl/core/ops/index/trait.Index.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/protocol/struct.ChunkRangesSeq.html\" title=\"struct iroh_blobs::protocol::ChunkRangesSeq\">ChunkRangesSeq</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.usize.html\">usize</a>&gt; for <a class=\"struct\" href=\"iroh_blobs/format/collection/struct.Collection.html\" title=\"struct iroh_blobs::format::collection::Collection\">Collection</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[847]}
