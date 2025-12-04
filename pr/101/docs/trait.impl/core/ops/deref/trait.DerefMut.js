5553d06c9f0c050ab9f758bce3bbe43da0fc428b
rklaehn <rklaehn@users.noreply.github.com>
2025-12-04 10:55:29 +0000

    deploy: 2d96ab7bb10963224be4f46d6d3bf0e004140be2

--- pr/101/docs/trait.impl/core/ops/deref/trait.DerefMut.js
+++ pr/101/docs/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"iroh_blobs/get/struct.Stats.html\" title=\"struct iroh_blobs::get::Stats\">Stats</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"iroh_blobs/provider/struct.ProgressReader.html\" title=\"struct iroh_blobs::provider::ProgressReader\">ProgressReader</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"iroh_blobs/provider/struct.ProgressWriter.html\" title=\"struct iroh_blobs::provider::ProgressWriter\">ProgressWriter</a>"]]]]);
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"iroh_blobs/get/struct.Stats.html\" title=\"struct iroh_blobs::get::Stats\">Stats</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[924]}
+//{"start":57,"fragment_lengths":[294]}
