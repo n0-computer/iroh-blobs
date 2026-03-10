bd7651b8f90d1a087d3b8f76d37bab2354090ea5
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-03-10 15:46:23 +0000

    deploy: 880c22eab6ce4c6537f89373a0d9020e90c4235c

---
+++ pr/212/docs/trait.impl/bao_tree/io/mixed/trait.ReadBytesAt.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl ReadBytesAt for <a class=\"struct\" href=\"iroh_blobs/store/mem/struct.DataReader.html\" title=\"struct iroh_blobs::store::mem::DataReader\">DataReader</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[181]}
---
+++ pr/212/docs/trait.impl/irpc/trait.Service.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl Service for <a class=\"enum\" href=\"iroh_blobs/provider/events/enum.ProviderProto.html\" title=\"enum iroh_blobs::provider::events::ProviderProto\">ProviderProto</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[192]}
