93f8049af519584465f503ce977a2ace215db4b1
rklaehn <rklaehn@users.noreply.github.com>
2026-04-17 15:06:04 +0000

    deploy: 91016d89ea2ed5e1733d3847c997db1c5f725d94

---
+++ pr/222/docs/trait.impl/bao_tree/io/sync/trait.Outboard.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl Outboard for <a class=\"struct\" href=\"iroh_blobs/store/mem/struct.OutboardReader.html\" title=\"struct iroh_blobs::store::mem::OutboardReader\">OutboardReader</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[190]}
---
+++ pr/222/docs/trait.impl/iroh_io/trait.AsyncStreamReader.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_blobs",[["impl&lt;R: <a class=\"trait\" href=\"iroh_blobs/util/trait.RecvStream.html\" title=\"trait iroh_blobs::util::RecvStream\">RecvStream</a>&gt; AsyncStreamReader for <a class=\"struct\" href=\"iroh_blobs/util/struct.RecvStreamAsyncStreamReader.html\" title=\"struct iroh_blobs::util::RecvStreamAsyncStreamReader\">RecvStreamAsyncStreamReader</a>&lt;R&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[372]}
