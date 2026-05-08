4aa011b4c93ca43943b981283ff8617e48a677c2
rklaehn <rklaehn@users.noreply.github.com>
2026-05-08 13:06:40 +0000

    deploy: 3f078afd5d48acec288ceaa0ccffb959bd75b455

---
+++ pr/230/docs/trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["iroh_blobs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"iroh_blobs/format/collection/struct.Collection.html\" title=\"struct iroh_blobs::format::collection::Collection\">Collection</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for <a class=\"struct\" href=\"iroh_blobs/hashseq/struct.HashSeq.html\" title=\"struct iroh_blobs::hashseq::HashSeq\">HashSeq</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[700]}
